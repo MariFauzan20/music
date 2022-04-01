e154846ab2aee50d4cb50eac654602cc88d08a02
Mar'i Fauzan <marifauzan20@gmail.com>
2022-04-01 15:56:27 +0700

    add state select

--- src/Components/Music/index.js
+++ src/Components/Music/index.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Music({ id, urlImg, title, artist, album }) {
+  const [select, setSelect] = useState(false);
+
+  const clickSelect = () => {
+    setSelect(!select);
+    console.log(select);
+  };
+
   return (
     <div key={id} className="col col-lg-3 col-md-4 col-sm-12 my-3">
       <div className="card p-3" style={{ maxWidth: 300, maxHeight: 600 }}>
@@ -8,8 +15,11 @@ export default function Music({ id, urlImg, title, artist, album }) {
         <h5 className="fs-5">{title}</h5>
         <h6 className="fw-normal">{artist}</h6>
         <p className="fw-light text-reset">{album}</p>
-        <button className="btn btn-primary" id="select">
-          Select
+        <button
+          className={`btn ${select ? "btn-success" : "btn-primary"}`}
+          onClick={clickSelect}
+        >
+          {`${select ? "Deselect" : "Select"}`}
         </button>
       </div>
     </div>
